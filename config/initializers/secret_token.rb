5db61c3507a062c90c82aa50fe1c39624a146211
Max Kramer <max@maxkramer.me>
2011-03-29 19:22:25 -0400

    Initial rails generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Medpass::Application.config.secret_token = '3cfd9950409227516f5acca2085b76410f52e9038414e2dd61f025aea7e45b4eb405a12255459b094af2ed3ee51f60782c973e2eab9bca26b71bc4df9bab5f1f'
