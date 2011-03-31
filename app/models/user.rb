a5df2a926acfee5d0170a2f9f8391622ddbc8223
Max Kramer <max@maxkramer.me>
2011-03-31 11:18:28 -0400

    Adding Spanish support

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
 class User < ActiveRecord::Base
 
+  # has_one :emergency_contact
+  
   has_many :allergies
   has_many :allergens, :through => :allergies
+  
+  # has_many :diagnoses
+  # has_many :conditions, :through => :diagnoses
+  
+  # has_many :procedures
+  # has_many :procedures, :through => :procedures
+  
+  # has_many :provider_assignments
+  # has_many :providers, :through => :provider_assignments
   
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
@@ -9,5 +20,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :language
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,10 @@ Medpass::Application.routes.draw do
 
   devise_for :users
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  match 'about' => 'page#about'
+  match 'contact' => 'page#contact'
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
+ 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
@@ -54,7 +51,7 @@ Medpass::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-root :to => "page#home"
+  root :to => "page#home"
 
   # See how all your routes lay out with "rake routes"
 
